106280acea88b9c2f4dd12817ad8d7134ba381c3
d10sfan <d10sfan@gmail.com>
2023-04-21 10:16:23 -0500

    ?

--- index.js
+++ index.js
@@ -39,16 +39,28 @@ async function getInstalledSystemPackages() {
     });
 }
 
+async function checkForVcpkgExists(name, version, vcpkgLibraries) {
+    try {
+        await fs.promises.access(path.join(vcpkgPortPath, name));
+        vcpkgLibraries.push({
+            name: name,
+            version: version
+        });
+        return true;
+    } catch (error) {
+        // The check failed
+    }
+}
+
 async function compareAgainstVcpkg(systemPackages) {
     const vcpkgLibraries = [];
     for(let systemPackage of systemPackages) {
         try {
-            console.log(path.join(vcpkgPortPath, systemPackage.name));
-            await fs.promises.access(path.join(vcpkgPortPath, systemPackage.name));
-            vcpkgLibraries.push({
-                name: systemPackage.name,
-                version: systemPackage.version
-            });
+            if(!await checkForVcpkgExists(systemPackage.name, systemPackage.version, vcpkgLibraries)) {
+                if(!await checkForVcpkgExists(systemPackage.name.split('-dev')[0], systemPackage.version, vcpkgLibraries)) {
+                    await checkForVcpkgExists(systemPackage.name.split('-dev')[0].replace('lib', ''), systemPackage.version, vcpkgLibraries)
+                }
+            }
         } catch (error) {
             // The check failed
         }
