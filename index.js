062beb602a04800533add3b0f94ac081015a286e
d10sfan <d10sfan@gmail.com>
2023-08-02 15:11:21 -0500

    Ignore glib

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const vcpkgPortPath = path.join(__dirname, 'vcpkg', 'ports');
 const overlaysPath = path.join(__dirname, 'overlays');
-const ignorePackages = ['sdl2-mixer', 'libvorbis', 'libogg', 'gettext'];
+const ignorePackages = ['sdl2-mixer', 'libvorbis', 'libogg', 'gettext', 'glib'];
 const foundPackagesKeys = {};
 
 async function getInstalledSystemPackages() {
