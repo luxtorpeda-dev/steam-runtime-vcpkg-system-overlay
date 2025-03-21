ef0206a1269f0042ad40113852563ed2f288d145
d10sfan <d10sfan@gmail.com>
2025-03-21 18:40:25 -0500

    remove freetype

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const vcpkgPortPath = path.join(__dirname, 'vcpkg', 'ports');
 const overlaysPath = path.join(__dirname, 'overlays');
-const ignorePackages = ['sdl2-mixer', 'libvorbis', 'libogg', 'gettext', "libwebp", "bzip2", "libflac", "opus", "opusfile", "gperf", "glslang", "libxau", "libxdmcp", "libxml2"];
+const ignorePackages = ['sdl2-mixer', 'libvorbis', 'libogg', 'gettext', "libwebp", "bzip2", "libflac", "opus", "opusfile", "gperf", "glslang", "libxau", "libxdmcp", "libxml2", "freetype"];
 const foundPackagesKeys = {};
 
 async function getInstalledSystemPackages() {
